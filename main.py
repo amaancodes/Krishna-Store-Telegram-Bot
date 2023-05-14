a4dc0727816055258c1060b5eba495b244417844
Amaan <amaanmulla07@gmail.com>
2023-05-14 20:33:11 +0530

    Update main.py
    Added two more commands to view orders and to become affiliates

--- main.py
+++ main.py
@@ -6,16 +6,19 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 
 print('Starting up bot...')
 
-TOKEN: Final = 'YOUR TOKEN'
-BOT_USERNAME: Final = '@your_bot_user'
+TOKEN: Final = '5875190732:AAH8eH9qfd661Wx4Z0TYUaLBDeOSp7b5s4'
+BOT_USERNAME: Final = '@Krishna_Stores_bot'
 
-
+orders = []
+affiliates = {}
 # Let us use the /start command
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     reply_text = 'Welcome to the Grocery Store Bot!\n\n' \
                  'What would you like to do?\n\n' \
                  '/inventory - View available products\n' \
-                 '/order - Place an order'
+                 '/order - Place an order'\
+                 '/vieworders - View all orders\n' \
+                 '/becomeaffiliate - Become an affiliate'
     await update.message.reply_text(reply_text)
 
 
@@ -64,6 +67,29 @@ async def process_order(update: Update, context: ContextTypes.DEFAULT_TYPE):
     reply_text = 'Your order has been placed. Thank you!'
     await update.message.reply_text(reply_text)
 
+async def view_orders_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if len(orders) > 0:
+        reply_text = 'All Orders:\n\n' + '\n'.join(orders)
+    else:
+        reply_text = 'No orders placed yet.'
+
+    await update.message.reply_text(reply_text)
+
+
+# Let us use the /becomeaffiliate command
+async def become_affiliate_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.message.from_user.id
+
+    # Generate a unique affiliate ID for the user
+    affiliate_id = str(uuid.uuid4())
+
+    # Store the affiliate ID in the affiliates dictionary
+    affiliates[user_id] = affiliate_id
+
+    reply_text = f'Congratulations! You are now an affiliate.\n\n' \
+                 f'Your unique affiliate ID is: {affiliate_id}\n\n' \
+                 f'Start sharing your affiliate link to earn commissions!'
+    await update.message.reply_text(reply_text)
 
 # Log errors
 async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -78,9 +104,12 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler('start', start_command))
     app.add_handler(CommandHandler('inventory', inventory_command))
     app.add_handler(CommandHandler('order', order_command))
+    app.add_handler(CommandHandler('vieworders', view_orders_command))
+    app.add_handler(CommandHandler('becomeaffiliate', become_affiliate_command))
+
 
     # Process the order when a message is received
-    app.add_handler(MessageHandler(filters.Filters.text, process_order))
+    app.add_handler(MessageHandler(filters.TEXT, process_order))
 
     # Log all errors
     app.add_error_handler(error)
