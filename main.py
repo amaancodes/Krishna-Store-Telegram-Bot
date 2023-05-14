d2119fa9693838e90fdeb71e5b5b492e15866063
Amaan <amaanmulla07@gmail.com>
2023-05-14 13:25:16 +0530

    updated the commands

--- main.py
+++ main.py
@@ -1,83 +1,26 @@
-import logging
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
-from telegram.ext import (
-    Updater,
-    CommandHandler,
-    MessageHandler,
-    Filters,
-    ConversationHandler,
-    CallbackContext,
-)
+from typing import Final
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-logger = logging.getLogger(__name__)
+# pip install python-telegram-bot
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
-# States of the conversation
-SELECTING_OPTION, ADDING_ITEMS, TAKING_ORDER = range(3)
+print('Starting up bot...')
 
-# Helper function to start the bot
-def start(update: Update, _: CallbackContext) -> int:
-    reply_keyboard = [['View Inventory', 'Take Order']]
+TOKEN: Final = 'YOUR TOKEN'
+BOT_USERNAME: Final = '@your_bot_user'
 
-    update.message.reply_text(
-        'Welcome to the Grocery Store Bot!\n\n'
-        'What would you like to do?',
-        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
-    )
 
-    return SELECTING_OPTION
+# Let us use the /start command
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    reply_text = 'Welcome to the Grocery Store Bot!\n\n' \
+                 'What would you like to do?\n\n' \
+                 '/inventory - View available products\n' \
+                 '/order - Place an order'
+    await update.message.reply_text(reply_text)
 
-# Function to handle the "View Inventory" command
-def view_inventory(update: Update, _: CallbackContext) -> int:
-    # Replace with your logic to fetch and display the inventory
-    inventory = get_inventory()  # Function to fetch the inventory
 
-    update.message.reply_text(
-        f'Current Inventory:\n\n{inventory}',
-        reply_markup=ReplyKeyboardRemove(),
-    )
-
-    return SELECTING_OPTION
-
-# Function to handle the "Take Order" command
-def take_order(update: Update, _: CallbackContext) -> int:
-    update.message.reply_text(
-        'Please enter the items you want to order, separated by commas:',
-        reply_markup=ReplyKeyboardRemove(),
-    )
-
-    return TAKING_ORDER
-
-# Function to handle the user's order
-def process_order(update: Update, _: CallbackContext) -> int:
-    order = update.message.text
-
-    # Replace with your logic to process the order
-    process_order(order)  # Function to process the order
-
-    update.message.reply_text('Your order has been placed. Thank you!')
-
-    return SELECTING_OPTION
-
-# Function to handle unknown commands
-def unknown(update: Update, _: CallbackContext) -> int:
-    update.message.reply_text('Sorry, I didn\'t understand that command.')
-
-    return SELECTING_OPTION
-
-# Function to handle errors
-def error(update: Update, context: CallbackContext) -> None:
-    logger.error(f'Update "{update}" caused error "{context.error}"')
-
-# Function to stop the bot
-def stop(update: Update, _: CallbackContext) -> int:
-    update.message.reply_text('Goodbye!')
-
-    return ConversationHandler.END
-
-def get_inventory():
+# Let us use the /inventory command
+async def inventory_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # Replace with your actual logic to fetch the inventory
     # This is just a sample implementation
 
@@ -93,9 +36,19 @@ def get_inventory():
     # Generate a formatted string for displaying the inventory
     inventory_str = '\n'.join([f'{item}: {quantity}' for item, quantity in inventory.items()])
 
-    return inventory_str
+    reply_text = f'Current Inventory:\n\n{inventory_str}'
+    await update.message.reply_text(reply_text)
 
-def process_order(order):
+
+# Let us use the /order command
+async def order_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Please enter the items you want to order, separated by commas:')
+
+
+# Process the user's order
+async def process_order(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    order = update.message.text
+
     # Replace with your actual logic to process the order
     # This is just a sample implementation
 
@@ -108,41 +61,30 @@ def process_order(order):
         # Here, we are just printing the item as a placeholder
         print(f"Processing item: {item}")
 
-    # You can also save the order details to a database or perform any other necessary actions
-
-    # This is just a sample implementation to show that the order has been processed
-    print("Order processed successfully")
+    reply_text = 'Your order has been placed. Thank you!'
+    await update.message.reply_text(reply_text)
 
 
-def main() -> None:
-    # Set up the bot
-    updater = Updater('YOUR_BOT_TOKEN')
-    dispatcher = updater.dispatcher
+# Log errors
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} caused error {context.error}')
 
-    # Define conversation handler
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
-        states={
-            SELECTING_OPTION: [
-                MessageHandler(Filters.regex('^View Inventory$'), view_inventory),
-                MessageHandler(Filters.regex('^Take Order$'), take_order),
-            ],
-            TAKING_ORDER: [MessageHandler(Filters.text, process_order)],
-        },
-        fallbacks=[CommandHandler('stop', stop)],
-    )
 
-    # Add conversation handler to the dispatcher
-    dispatcher.add_handler(conv_handler)
+# Run the program
+if __name__ == '__main__':
+    app = Application.builder().token(TOKEN).build()
+
+    # Commands
+    app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('inventory', inventory_command))
+    app.add_handler(CommandHandler('order', order_command))
+
+    # Process the order when a message is received
+    app.add_handler(MessageHandler(filters.Filters.text, process_order))
 
     # Log all errors
-    dispatcher.add_error_handler(error)
+    app.add_error_handler(error)
 
-    # Start the bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT, SIGTERM or SIGABRT
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+    print('Polling...')
+    # Run the bot
+    app.run_polling(poll_interval=5)
