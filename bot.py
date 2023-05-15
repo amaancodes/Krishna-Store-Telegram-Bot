44919ce6842b8b1cb25d3b48c6e3fa2a89779d4b
Amaan <amaanmulla07@gmail.com>
2023-05-15 12:35:13 +0530

    update bot.py
    added exit command

--- bot.py
+++ bot.py
@@ -1,126 +1,149 @@
 import logging
-import random
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, ConversationHandler, MessageHandler, Filters
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
-
 logger = logging.getLogger(__name__)
 
-# Global variables to store products and orders
-products = {'Apple': 1.5, 'Banana': 0.5, 'Orange': 1.0, 'Mango': 2.0}
+# Global variables to store product data and user orders
+products = {
+    'Apple': 1.0,
+    'Banana': 0.5,
+    'Orange': 0.75,
+    'Mango': 1.5
+}
 orders = {}
 affiliates = {}
 
+# Start command
 def start(update, context):
-    """Start the bot and display the available commands"""
-    user = update.message.from_user
-    update.message.reply_text(f"Hello {user.first_name}! I'm a grocery store bot. How can I assist you?"
-                              "\n\nYou can use the following commands:"
-                              "\n- /viewproducts: View available products"
-                              "\n- /order: Place an order"
-                              "\n- /vieworder: View your current order"
-                              "\n- /becomeaffiliate: Become an affiliate")
+    update.message.reply_text(
+        "Welcome to the grocery store bot!\n"
+        "You can use the following commands:\n"
+        "/viewproducts - View available products\n"
+        "/order - Place an order\n"
+        "/vieworder - View your order\n"
+        "/becomeaffiliate - Become an affiliate\n"
+        "/exit - Exit the bot"
+    )
 
+# View products command
 def view_products(update, context):
-    """Display the available products with their prices"""
-    product_list = "Available products:\n"
-    for product, price in products.items():
-        product_list += f"{product} - ${price}\n"
-    update.message.reply_text(product_list)
+    products_list = "\n".join([f"{product} - ${price}" for product, price in products.items()])
+    update.message.reply_text(f"Available products:\n{products_list}")
+
+# Order conversation
+def order_start(update, context):
+    update.message.reply_text("Please enter the product name and quantity (e.g., Apple 5). "
+                              "Type /done to finish ordering.")
+    return "ordering"
 
 def order(update, context):
-    """Start the ordering process and add quantity for each product"""
-    user = update.message.from_user
-    update.message.reply_text(f"{user.first_name}, please enter the products you want to order along with the quantity "
-                              "(e.g., 'Apple 2', 'Banana 3'):")
+    message = update.message.text.strip().split()
+    if len(message) != 2:
+        update.message.reply_text("Invalid format. Please enter the product name and quantity.")
+        return "ordering"
+    
+    product_name = message[0]
+    quantity = int(message[1])
+    
+    if product_name not in products:
+        update.message.reply_text("Invalid product name. Please try again.")
+        return "ordering"
+    
+    if quantity <= 0:
+        update.message.reply_text("Invalid quantity. Please enter a positive value.")
+        return "ordering"
+    
+    if update.message.from_user.id not in orders:
+        orders[update.message.from_user.id] = {}
+    
+    orders[update.message.from_user.id][product_name] = quantity
+    update.message.reply_text("Product added to your order.")
+    
+    return "ordering"
 
-def process_order(update, context):
-    """Process the customer's order and calculate the total bill amount"""
-    user = update.message.from_user
-    order_items = update.message.text.split(',')
-
-    if user.id not in orders:
-        orders[user.id] = {}
-
-    total_bill = 0.0
-
-    for item in order_items:
-        item = item.strip().split()
-        if len(item) == 2:
-            product_name = item[0]
-            quantity = int(item[1])
-            if product_name in products:
-                price = products[product_name]
-                total_price = price * quantity
-                total_bill += total_price
-
-                if product_name in orders[user.id]:
-                    orders[user.id][product_name] += quantity
-                else:
-                    orders[user.id][product_name] = quantity
-            else:
-                update.message.reply_text(f"Sorry, '{product_name}' is not available.")
-
-    update.message.reply_text(f"{user.first_name}, your order has been placed. "
-                              f"Total bill amount: ${total_bill:.2f}")
+def order_done(update, context):
+    total_amount = 0
+    order_details = ""
+    
+    if update.message.from_user.id in orders:
+        for product_name, quantity in orders[update.message.from_user.id].items():
+            price = products[product_name]
+            amount = price * quantity
+            total_amount += amount
+            order_details += f"{product_name} x{quantity} - ${amount}\n"
+        
+        if total_amount > 0:
+            update.message.reply_text(f"Order details:\n{order_details}\nTotal amount: ${total_amount}")
+        else:
+            update.message.reply_text("Your order is empty.")
+    else:
+        update.message.reply_text("Your order is empty.")
+    
+    return ConversationHandler.END
 
 def view_order(update, context):
-    """Display the ordered items with the total bill amount"""
-    user = update.message.from_user
-    if user.id in orders:
-        order_items = orders[user.id]
-        if order_items:
-            order_list = "Your order:\n"
-            total_bill = 0.0
-            for product_name, quantity in order_items.items():
-                price = products[product_name]
-                total_price = price * quantity
-                order_list += f"{product_name} - {quantity} x ${price} = ${total_price:.2f}\n"
-                total_bill += total_price
+    if update.message.from_user.id in orders:
+        order_details = ""
+        total_amount = 0
 
-            order_list += f"Total bill amount: ${total_bill:.2f}"
-            update.message.reply_text(order_list)
+        for product_name, quantity in orders[update.message.from_user.id].items():
+            price = products[product_name]
+            amount = price * quantity
+            total_amount += amount
+            order_details += f"{product_name} x{quantity} - ${amount}\n"
+
+        if order_details:
+            order_details += f"\nTotal amount: ${total_amount}"
+            update.message.reply_text(f"Your order:\n{order_details}")
         else:
-                        update.message.reply_text("You have not placed any orders yet.")
+            update.message.reply_text("Your order is empty.")
     else:
-        update.message.reply_text("You have not placed any orders yet.")
+        update.message.reply_text("Your order is empty.")
 
+# Become affiliate command
 def become_affiliate(update, context):
-    """Generate a unique affiliate ID for users who want to become affiliates"""
-    user = update.message.from_user
-    affiliate_id = random.randint(1000, 9999)
-    affiliates[user.id] = affiliate_id
-    update.message.reply_text(f"{user.first_name}, congratulations! You have become an affiliate."
-                              f"Your affiliate ID is: {affiliate_id}")
+    user_id = update.message.from_user.id
+    if user_id not in affiliates:
+        affiliate_id = len(affiliates) + 1
+        affiliates[user_id] = affiliate_id
+        update.message.reply_text(f"You are now an affiliate. Your affiliate ID is {affiliate_id}.")
+    else:
+        update.message.reply_text("You are already an affiliate.")
+
+# Exit command
+def exit_bot(update, context):
+    update.message.reply_text("Thank you for using the grocery store bot. Have a great day!")
+    return ConversationHandler.END
 
 def main():
-    """Start the bot"""
-    # Set up the Telegram Bot token
-    updater = Updater("YOUR_TELEGRAM_BOT_TOKEN", use_context=True)
+    # Create the Updater and Dispatcher
+    updater = Updater("YOUR_BOT_TOKEN", use_context=True)
+    dispatcher = updater.dispatcher
 
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
+    # Add conversation handler for the order flow
+    order_conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("order", order_start)],
+        states={
+            "ordering": [MessageHandler(Filters.text & ~Filters.command, order)],
+        },
+        fallbacks=[CommandHandler("done", order_done)],
+    )
+    dispatcher.add_handler(order_conv_handler)
 
-    # Add handlers for commands
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("viewproducts", view_products))
-    dp.add_handler(CommandHandler("order", order))
-    dp.add_handler(CommandHandler("vieworder", view_order))
-    dp.add_handler(CommandHandler("becomeaffiliate", become_affiliate))
-
-    # Add handler for messages
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, process_order))
+    # Add command handlers
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("viewproducts", view_products))
+    dispatcher.add_handler(CommandHandler("vieworder", view_order))
+    dispatcher.add_handler(CommandHandler("becomeaffiliate", become_affiliate))
+    dispatcher.add_handler(CommandHandler("exit", exit_bot))
 
     # Start the bot
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C
     updater.idle()
-
 
 if __name__ == '__main__':
     main()
-
